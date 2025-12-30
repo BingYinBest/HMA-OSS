1a1ad18de92e9a499683c4d88912cd8b5e5ad18c
BingYinBest <dreamfy66@outlook.com>
2025-12-31 06:16:07 +0800

    Update AppDataIsolationHook.kt

--- xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/hook/AppDataIsolationHook.kt
+++ xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/hook/AppDataIsolationHook.kt
@@ -13,7 +13,7 @@ import icu.nullptr.hidemyapplist.xposed.HMAService
 import icu.nullptr.hidemyapplist.xposed.logD
 import icu.nullptr.hidemyapplist.xposed.logE
 import icu.nullptr.hidemyapplist.xposed.logI
-import org.frknkrc44.hma_oss.common.BuildConfig
+import com.miui.video.common.BuildConfig
 
 @RequiresApi(Build.VERSION_CODES.R)
 class AppDataIsolationHook(private val service: HMAService): IFrameworkHook {
